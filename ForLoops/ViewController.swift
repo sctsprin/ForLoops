4f5550b52dccf4ad4151987c43f39006de33b4e5
James Springfield <sctsprin@me.com>
2014-09-24 16:50:35 +1000

    First Commit

--- ForLoops/ViewController.swift
+++ ForLoops/ViewController.swift
@@ -13,8 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+//        var meditationHours = 1
+//        for meditationHours; meditationHours < 100; meditationHours = meditationHours + 1 {
+//            println("I am getting more enlightened")
+//        }
 
+//        for var meditationHours = 1; meditationHours < 100; meditationHours++ {
+//            println("i am getting more enlightened \(meditationHours)")
+//            }
+//    }
+        
+        var wheat = 1
+        for var i = 0; i < 64; ++i {
+            wheat = wheat * 2
+            println("\(i) pieces of wheat \(wheat)")
+            
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
